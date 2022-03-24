5fd7e8d3da050b8d40392de8296a73167733f040
Ashbee Morgado <ashbee.morgado@icloud.com>
2022-03-24 10:24:14 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/test', function(){
+    return ['message'=>'get'];
+});
+
+Route::post('/test', function(){
+    return ['message'=>'get'];
+});
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
