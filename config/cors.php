209ec237ad4debb29447b28f837ffde8ee71b618
Ashbee Morgado <ashbee.morgado@icloud.com>
2022-05-19 11:29:53 +0800

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*', 'localhost:8100'],
+    'allowed_origins' => ['*', 'http://localhost:8100'],
 
     'allowed_origins_patterns' => [],
 
