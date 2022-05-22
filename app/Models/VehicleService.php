c439d473e3ad1b3ce875a33d66cb58376352b8ef
Ashbee Morgado <ashbee.morgado@icloud.com>
2022-05-22 13:03:47 +0800

    vehicle keys

--- app/Models/VehicleService.php
+++ app/Models/VehicleService.php
@@ -6,13 +6,20 @@ use App\Models\Service;
 use App\Models\Provider;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
 
 class VehicleService extends Model
 {
     use HasFactory;
     protected $guarded = [];
 
-
+    public static function boot()
+    {
+        parent::boot();
+        static::creating(function($model){
+            $model->key = Str::uuid();
+        });
+    }
     public function provider()
     {
         return $this->belongsTo(Provider::class);
