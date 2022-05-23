0f0dff89ab6bacfdaf73400e702d5e70fc57e3f6
Ashbee Morgado <ashbee.morgado@icloud.com>
2022-05-23 21:00:11 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11816,13 +11816,17 @@ section#contact form#contactForm :-ms-input-placeholder {
                         @if (Route::has('login'))
                 <!-- <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block"> -->
                     @auth
-                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                        <li class="nav-item">
+                          </li><a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                        </li>
                     @else
+                      <li class="nav-item">
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+                      </li>
 
-                        @if (Route::has('register'))
+                        {{-- @if (Route::has('register'))
                             <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-                        @endif
+                        @endif --}}
                     @endauth
                 <!-- </div> -->
             @endif
