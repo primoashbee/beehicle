95bc392eedb12ded1de4d41e6a29f010e265cddb
Ashbee Morgado <ashbee.morgado@icloud.com>
2022-05-23 21:02:58 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11817,7 +11817,7 @@ section#contact form#contactForm :-ms-input-placeholder {
                 <!-- <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block"> -->
                     @auth
                         </li class="nav-item">
-                          <a href="{{ route('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                          <a href="{{ route('dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
                         </li>
                     @else
                       <li class="nav-item">
