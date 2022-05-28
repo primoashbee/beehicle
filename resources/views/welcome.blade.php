de2f990d4ef8c30da8c20483c0b60df811adcc4e
Aaron <201510539@gordoncollege.edu.ph>
2022-05-28 13:34:38 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12331,10 +12331,10 @@ section#contact form#contactForm :-ms-input-placeholder {
                             <div class="col-lg-8">
                                 <div class="modal-body">
                                     <!-- Project details-->
-                                    <h2 class="text-uppercase">The Transaction Page</h2>
+                                    <h2 class="text-uppercase">Travel Record Page</h2>
                                     <p class="item-intro text-muted"></p>
-                                    <img class="img-fluid d-block mx-auto" src="css/assets/img/portfolio/transaction1.png" alt="..." />
-                                    <p>Beehicle's Transaction page, It will help track how much the vehicle will cost per travel, it can also help to compute the fuel consumption from point to point</p>
+                                    <img class="img-fluid d-block mx-auto" src="css/assets/img/portfolio/travel_record.png" alt="..." />
+                                    <p>it helps to know and recall the records you traveled,also provide the list for every kilometer reading that needs to PMS</p>
                                     <ul class="list-inline">
                                         
                                     </ul>
