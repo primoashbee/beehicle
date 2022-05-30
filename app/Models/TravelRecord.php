52f3efceb12061947405db4168fb7eceab95aa82
Ashbee Morgado <ashbee.morgado@icloud.com>
2022-05-30 21:50:42 +0800

    wip

--- app/Models/TravelRecord.php
+++ app/Models/TravelRecord.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use App\Models\Vehicle;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TravelRecord extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     protected $guarded = [];
     protected $dates = [
         'datetime',
