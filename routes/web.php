379797c0ed7d293f6ae505db6f1e1a406d0fddbd
Ashbee Morgado <ashbee.morgado@icloud.com>
2022-06-01 21:50:32 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -40,7 +40,7 @@ Route::get('/notice', function(){
 Route::get('/email/verify/{id}/{hash}', function ($id, $hash) {
     $user = User::find($id)->markEmailAsVerified();
     event(new Verified($user));
-    return redirect('/dashboard');
+    return redirect()->away("https://beehicle.xyz/welcome");
 })->name('verification.verify');
 
 
